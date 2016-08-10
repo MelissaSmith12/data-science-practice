be60a1ec5b1b4c04fc12086fb3c1c68f58592ec8
MelissaSmith12 <MelissaSmith12@gmail.com>
2016-08-09 23:08:03 -0400

    Initial upload, read 500 rows of All Item Report

---
+++ AllItemProofing.R
@@ -0,0 +1,33 @@
+#This program performs a range of content-specific data checks on data exported from
+#an educational database, ranging from simple data scrubbing tasks to content
+#validity. The final output is a flatlist that identifies the field which needs
+#to be changed and a description of the change needed.
+
+#check whether packages are installed
+
+
+#load libraries
+library(dplyr)
+
+#Set filenames and change working directory
+directory <- "C:/Users/Melissa/Downloads"
+setwd(directory) 
+CurrentDelivery <- "FAIB-allallItem04082016.csv"
+
+currentReport <- read.csv2(CurrentDelivery, header = TRUE, sep = ",", quote = "\"", nrows = 500)
+
+#Select Active Items
+currentActive <- filter(currentReport, Item.Status == "Active")
+
+#Select Active or in progress
+currentNotRetired <- filter(currentReport, Item.Status == "Active" | Item.Status == "In Progress")
+
+
+
+
+#Convert the Created.Date and Last.Modified.By columns
+
+
+#Find item codes where the item code contains a space
+
+

0cf0cd999269e1bc64370c1eccd1d7e730add131
MelissaSmith12 <MelissaSmith12@gmail.com>
2016-08-20 23:26:23 -0400

    Add summary charts and wordcloud

--- strengths.R
+++ strengths.R
@@ -1,13 +1,20 @@
 #This code reads the keywords from my peer-solicited strengths analysis and 
-#graphs trends and displays a summary chart
+#graphs trends and displays a summary chart and word cloud
 
+library(plyr)
 library(dplyr)
-
+library(wordcloud)
 
 #Set filenames and change working directory
 dir <- "C:/Users/Melissa/Downloads"
-directory <- "C:/Users/Melissa/Downloads"
-
-
+setwd(dir)
 file <- "strengths.txt"
-strengths <- read.csv(file, header = FALSE, sep = ",")
+Strengths <- read.csv(file, header = TRUE, sep = ",")
+StrengthsCount <- count(Strengths, c("Theme.word"))
+StrengthsCount
+
+highstrength <- filter(StrengthsCount, freq > 1)
+highstrength
+
+wordcloud(StrengthCorpus, max.words = 100, random.order = FALSE)
+wordcloud(StrengthsCount$Theme.word, StrengthsCount$freq, max.words=100, rot.per = .5, random.order = TRUE, random.color = TRUE)

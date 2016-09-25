7bebb6a97e65f402aa549eae69a1a1059de09ff9
MelissaSmith12 <MelissaSmith12@gmail.com>
2016-09-25 00:49:05 -0400

    Initial exploration

--- ItemWritingAnalysis.R
+++ ItemWritingAnalysis.R
@@ -5,4 +5,15 @@
 # This analysis seeks to ask the following questions: How long do cards stay in each phase? How many cycles
 # does the average card go through? Are there any users at any stage in the process that are outliers?
 
-# This project also provides an opportunity to play with APIs.
+# This project also provides an opportunity to play with APIs by playing with Trello
+library("httr")
+library("jsonlite")
+library("trelloR")
+
+token <- "a75a3e4d750fc6bdc912fd3de8c1f58c6fd706fab18dc54ce1072fb62fdf8e4d"
+
+#initial data exploration on personal board
+board = get_id_board("https://trello.com/b/HEgOoY9G/home-sprint-3")
+cards = get_board_cards(board)
+
+class(cards)

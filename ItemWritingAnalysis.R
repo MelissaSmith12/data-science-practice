323b218010ad92d9a5c59d2d50e22150cd741bdf
MelissaSmith12 <MelissaSmith12@gmail.com>
2016-09-25 00:57:19 -0400

    trying to work through passing token using trelloR

--- ItemWritingAnalysis.R
+++ ItemWritingAnalysis.R
@@ -10,10 +10,12 @@ library("httr")
 library("jsonlite")
 library("trelloR")
 
-token <- "a75a3e4d750fc6bdc912fd3de8c1f58c6fd706fab18dc54ce1072fb62fdf8e4d"
+my_token <- "a75a3e4d750fc6bdc912fd3de8c1f58c6fd706fab18dc54ce1072fb62fdf8e4d"
 
 #initial data exploration on personal board
-board = get_id_board("https://trello.com/b/HEgOoY9G/home-sprint-3")
+get_my_boards(token = my_token)
+board= get_id_board("https://trello.com/b/HEgOoY9G/home-sprint-3")
 cards = get_board_cards(board)
 
-class(cards)
+write.csv(cards, "trello.csv")
+

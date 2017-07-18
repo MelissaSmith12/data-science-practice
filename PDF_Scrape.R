7776a1b6e290e6bdd86c98806d18b2b71102b046
MelissaSmith12 <MelissaSmith12@gmail.com>
2017-07-18 00:08:59 -0400

    Delete PDF_Scrape.R

--- PDF_Scrape.R
+++
@@ -1,6 +0,0 @@
-# This file plays around with consuming data from PDF files for text mining
-# Source link is: http://data.library.virginia.edu/reading-pdf-files-into-r-for-text-mining/
-
-install.packages("tm") # only need to do once
-library(tm)
-
